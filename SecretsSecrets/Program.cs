606ccaa3e3625e4befde2e490c08ddc06ef8429c
Luis Fraile <lfraile@lfraile.net>
2021-10-02 18:08:58 +0200

    storage

---
+++ SecretsSecrets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SecretsSecrets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=aunsecuredstoragelf;AccountKey=E4NiFw9T8zXZWiSTVf6XCTjCnJH6Pfzry+rca1CQIohtW2qShXKUs9810qn0mEZds+GuKpW01cr8u4NklJGh8A==;EndpointSuffix=core.windows.net";
+            Console.WriteLine("Hello World!");
+
+            Azure.Storage.Blobs.BlobClient client = new Azure.Storage.Blobs.BlobClient(storageConnectionString, "test", "test");
+        }
+    }
+}
